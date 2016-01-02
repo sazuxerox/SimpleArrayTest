e4925f3bd63e8dd6501cca74e81b571ac8bcc13c
sazuxerox <sazzadhossain@live.com>
2016-01-03 00:57:42 +0600

    Initial commit

---
+++ GradeTest/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace GradeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var ts = new TestArrayListBed();
+
+            ts.Display();
+            double average = ts.Average();
+            Console.WriteLine("Average Grade is: {0}", average);
+
+            double highestGrade = ts.HighestGrade();
+            Console.WriteLine("Highest Grade is: {0}", highestGrade);
+
+            double lowestGrade = ts.LowestGrade();
+            Console.WriteLine("Lowest Grade is : {0}", lowestGrade);
+        }
+
+    }
+}
---
+++ GradeTest/TestArrayListBed.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Collections;
+
+namespace GradeTest
+{
+    class TestArrayListBed
+    {
+        
+        int[] grades = new int[]{78,85,86,80,75};
+        
+        public double Average()
+        {
+            int total = 0;
+            int len = grades.Length;
+            for (int i = 0; i < len; i++)
+            {
+                total += grades[i];
+            }
+            double average = (double)total/len;
+            return average;
+        }
+
+        public void Display()
+        {
+            int total = 0;
+            for (int i = 0; i <= grades.GetUpperBound(0); i++)
+            {
+                total += grades[i];
+                Console.Write(grades[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Total is : {0}", total);
+            Console.WriteLine();
+        }
+        public int HighestGrade()
+        {
+            int highestGrade = grades[0];
+            for (int i = 0; i <= grades.GetUpperBound(0); i++)
+            {
+                if (grades[i] > highestGrade)
+                {
+                    highestGrade = grades[i];
+                }
+            }
+            return highestGrade;
+        }
+        public int LowestGrade()
+        {
+            int lowestGrade = grades[0];
+            for (int i = 0; i <= grades.GetUpperBound(0); i++)
+            {
+                if (grades[i] < lowestGrade)
+                {
+                    lowestGrade = grades[i];
+                }
+            }
+            return lowestGrade;
+        }
+    }
+}
