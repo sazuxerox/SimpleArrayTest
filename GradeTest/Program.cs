bb352a6bb3f9da81154d77f27f94e347023ff0bd
sazuxerox <sazzadhossain@live.com>
2016-01-03 01:09:20 +0600

    Documentation added in the programs.

--- GradeTest/Program.cs
+++ GradeTest/Program.cs
@@ -7,15 +7,21 @@ namespace GradeTest
     {
         static void Main(string[] args)
         {
-          var ts = new TestArrayListBed();
+            //declare an object of TestArrayListBed class to access it.
+            var ts = new TestArrayListBed();
 
+            //display the array values.
             ts.Display();
+
+            //display the average value of the array
             double average = ts.Average();
             Console.WriteLine("Average Grade is: {0}", average);
 
+            //display the highest value of the array
             double highestGrade = ts.HighestGrade();
             Console.WriteLine("Highest Grade is: {0}", highestGrade);
 
+            //display the lowest value of the array. 
             double lowestGrade = ts.LowestGrade();
             Console.WriteLine("Lowest Grade is : {0}", lowestGrade);
         }
--- GradeTest/TestArrayListBed.cs
+++ GradeTest/TestArrayListBed.cs
@@ -1,26 +1,34 @@
 ﻿
 using System;
-using System.Collections;
 
 namespace GradeTest
 {
     class TestArrayListBed
     {
-        
-        int[] grades = new int[]{78,85,86,80,75};
-        
+
+        int[] grades = new int[] { 78, 85, 86, 80, 75 };
+
+        //find the average of the array values
         public double Average()
         {
             int total = 0;
+
+            //get the length of the array
             int len = grades.Length;
+
+            //traveres through the array.
             for (int i = 0; i < len; i++)
             {
+                //find the total of the array values.
                 total += grades[i];
             }
-            double average = (double)total/len;
+            //find the average calculation by dividing the total by the 
+            //number of array values.
+            double average = (double)total / len;
             return average;
         }
 
+        //Display the array values and the total
         public void Display()
         {
             int total = 0;
@@ -33,29 +41,42 @@ namespace GradeTest
             Console.WriteLine("Total is : {0}", total);
             Console.WriteLine();
         }
+
+
+        //find the highest value of the array
         public int HighestGrade()
         {
+            //assign the first array values to the variable
             int highestGrade = grades[0];
             for (int i = 0; i <= grades.GetUpperBound(0); i++)
             {
+                //check if the traveresed value is greater than the previous value
                 if (grades[i] > highestGrade)
                 {
+                    //assigning the new highest value to the declared variable
                     highestGrade = grades[i];
                 }
             }
-            return highestGrade;
+            return highestGrade;  //return the value
         }
+
+        //find the lowest value of the array
         public int LowestGrade()
         {
+            //assign the first array values to the variable
             int lowestGrade = grades[0];
+
+            //traversing through the array
             for (int i = 0; i <= grades.GetUpperBound(0); i++)
             {
+                //checking if the current traversed value is smaller than the assigned value in the variable.
                 if (grades[i] < lowestGrade)
                 {
+                    //assigning the new smallest value to the variable
                     lowestGrade = grades[i];
                 }
             }
-            return lowestGrade;
+            return lowestGrade;  //return the value
         }
     }
 }
